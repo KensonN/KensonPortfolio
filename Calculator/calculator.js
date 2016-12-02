e352d1b08d4ef59ffb0acefa5844cfe22a83b1f5
Kenson Nguyen <knguyen.ra@mysummitps.org>
2016-12-02 18:31:39 +0000

    Shortened calculator code

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -13,64 +13,39 @@ calculate.addEventListener("click", function(){
 //We use parseInt so that the numbers are added instead of put next to each other in a string
     if (selected==="+") {
         sum = parseInt(number1.value) + parseInt(number2.value);
-//1.797693134862316E+308 is the maximum value javascript can handle. After that, it will just display infinity.
-//If the sum is larger than infinity, display "Number is too large"
-//If the sum is less than or equal to infinity, display the sum
-        if (sum === Infinity || sum === -Infinity ) {
-            document.getElementById("Answer").innerHTML = "Number too big! Try again";
-        }
-        else document.getElementById("Answer").innerHTML = sum;
     }
 //If the minus sign is chosen, subtract
     if (selected==="-") {
         sum = parseInt(number1.value) - parseInt(number2.value);
-//Same if statement as lines 19-23
-
-        if (sum === Infinity || sum === -Infinity ) {
-            document.getElementById("Answer").innerHTML = "Number too big! Try again";
-        }
-        else document.getElementById("Answer").innerHTML = sum;
-
     }
 //If the multiply sign is chosen, multiply
     if (selected==="*") {
         sum = parseInt(number1.value) * parseInt(number2.value);
-//Same if statement as lines 19-23
-        if (sum === Infinity || sum === -Infinity ) {
-            document.getElementById("Answer").innerHTML = "Number too big! Try again";
-        }
-        else document.getElementById("Answer").innerHTML = sum;
     }
 //If the division sign is chosen, divide
     if (selected==="/") {
         sum = parseInt(number1.value) / parseInt(number2.value);
-//Same if statement as lines 19-23
-        if (sum === Infinity || sum === -Infinity ) {
-            document.getElementById("Answer").innerHTML = "Number too big! Try again";
-        }
-        else document.getElementById("Answer").innerHTML = sum;   
     }
 //If up caret is chosen, use exponent function
     if (selected==="^") {
         sum = Math.pow(number1.value, number2.value);
-//Same if statement as lines 19-23
-        if (sum === Infinity || sum === -Infinity ) {
-            document.getElementById("Answer").innerHTML = "Number too big! Try again";
-        }
-        else document.getElementById("Answer").innerHTML = sum;
     }
 //If Nth root is selected, use the Nth root function
     if (selected==="nth root") {
         sum = Math.pow(number2.value, 1/number1.value);
-//Same if statement as lines 19-23
-        if (sum === Infinity || sum === -Infinity ) {
-            document.getElementById("Answer").innerHTML = "Number too big! Try again";
+    }     
+//1.797693134862316E+308 is the maximum value javascript can handle. After that, it will just display infinity.
+//If the sum is larger than infinity, display "Number is too large"
+//If the sum is less than or equal to infinity, display the sum
+    if (sum === Infinity || sum === -Infinity ) {
+    document.getElementById("Answer").innerHTML = "Number too big! Try again";
         }
-        else document.getElementById("Answer").innerHTML = sum; 
+    else {
+    document.getElementById("Answer").innerHTML = sum;
+    }
 //Javascript cannnot handle negative numbers with the nth root function
 //Because of this, the calculator will display negative numbers are not supported if it returns NaN
-        if (document.getElementById("Answer").innerHTML === "NaN") {
-            document.getElementById("Answer").innerHTML = "Negative numbers are not supported"
-        }
+    if (document.getElementById("Answer").innerHTML === "NaN") {
+    document.getElementById("Answer").innerHTML = "Negative numbers are not supported!"
     }
 });
